ddcc5ed6b23c271f0891790ae087d777618b4ca7
Youssef W. Soliman <ywsoliman@gmail.com>
2022-05-08 05:28:43 +0200

    Create A - Candies and Two Sisters.cpp

---
+++ A - Candies and Two Sisters.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n; cin >> n;
+    for (int i = 0; i < n; i++) {
+        int num; cin >> num;
+        if (num % 2 == 0)
+            cout << num / 2 - 1 << endl;
+        else
+            cout << num / 2 << endl;
+    }
+    return 0;
+}
