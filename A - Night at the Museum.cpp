93acd4b2cbdbfa2392739614396c987ed824150d
Youssef W. Soliman <ywsoliman@gmail.com>
2022-05-19 07:33:16 +0200

    Update A - Night at the Museum.cpp

--- A - Night at the Museum.cpp
+++ A - Night at the Museum.cpp
@@ -4,17 +4,12 @@ using namespace std;
 
 int main()
 {
-    char letter[26] = {
-            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
-            'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-            't', 'u', 'v', 'w', 'x','y', 'z'
-    };
     string s; cin >> s;
-    int x = 0;
+    char x = 'a';
     int total = 0;
     for (char c : s) {
-        total += min(abs(c - letter[x]), 26 - abs(c - letter[x]));
-        x = c - 'a';
+        total += min(abs(c - x), 26 - abs(c - x));
+        x = c;
     }
     cout << total;
 
