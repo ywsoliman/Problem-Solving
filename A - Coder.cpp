e6a0b285c48ea9ce0c2d1920359158de1cf6c365
Youssef W. Soliman <ywsoliman@gmail.com>
2022-07-18 00:29:30 +0200

    Create A - Coder.cpp

---
+++ A - Coder.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n; cin >> n;
+    if (n % 2 == 0)
+        cout << n * n / 2 << "\n";
+    else
+        cout << (n * n / 2) + 1 << "\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i % 2 == 0) {
+                if (j % 2 == 0)
+                    cout << "C";
+                else
+                    cout << ".";
+            }
+            else {
+                if (j % 2 == 0)
+                    cout << ".";
+                else
+                    cout << "C";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
